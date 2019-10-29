18bfded1bb8be6eac31668bbddc2260f662298e7
Nsuliman <nsuliman88@gmail.com>
2019-10-29 17:35:43 +0200

    drop down list for selected movie type

--- js/Booking-Update.js
+++ js/Booking-Update.js
@@ -2,23 +2,24 @@
 
 var movieForm = document.getElementById('formArea-Movies');
 var movieD = document.getElementById('Movie-Details');
-var hh = 'Romantic';
+
 var seatCounter = 0;
+
 newArray = [];   /// To show the user data 
 
-romanticMo = ['After', 'Downton Abbey', 'Maleficent', 'Aladdin', 'Judy (II)'];
-console.log('romanticMo , romanticMo.length : ', romanticMo, romanticMo.length);
+romanticMo = ['ROMANTIC LIST','After', 'Downton Abbey', 'Maleficent', 'Aladdin', 'Judy (II)'];
+//console.log('romanticMo , romanticMo.length : ', romanticMo, romanticMo.length);
 
-horrorMo = ['The Exorcist', 'Psycho', 'Halloween', 'The Thing', 'The Fly'];
+horrorMo = ['HORROR LIST','The Exorcist', 'Psycho', 'Halloween', 'The Thing', 'The Fly'];
 //console.log('horrorMo , horrorMo.length : ', horrorMo , horrorMo.length);
 
-comedyMo = ['Death at a Funeral', 'Step Brothers', 'The Hangover', 'Superbad', 'Project X'];
+comedyMo = ['COMEDY LIST','Death at a Funeral', 'Step Brothers', 'The Hangover', 'Superbad', 'Project X'];
 //console.log('comedyMo , comedyMo.length : ', comedyMo , comedyMo.length);
 
-actionMo = ['The Equalizer', 'Logan', 'The Avengers', 'Captain Marvel', 'Black Panther'];
+actionMo = ['ACTION LIST','The Equalizer', 'Logan', 'The Avengers', 'Captain Marvel', 'Black Panther'];
 //console.log('actionMo , actionMo.length : ', actionMo , actionMo.length);
 
-dramaMo = ['Joker', 'Gemini Man', 'The Lion King', 'The Irishman', 'IT 2'];
+dramaMo = ['DRAMA LIST','Joker', 'Gemini Man', 'The Lion King', 'The Irishman', 'IT 2'];
 // console.log('dramaMo , dramaMo.length : ', dramaMo , dramaMo.length);
 
 
@@ -35,9 +36,6 @@ function MoviesKind(name, moviesSelect, seatLoc, seatsNum, payMethod) {
     MoviesKind.moviesList.push(this);
 }
 MoviesKind.moviesList = [];                 // Empty array for user choice 
-
-new MoviesKind('', 'After', '', 0, ' at 6pm ');
-
 
 //////////// Store the user's data 
 function updateMovies() {
@@ -82,7 +80,7 @@ function renderMovie() {
     element.textContent = " Welcome Mr " + newArray[0] + 'You Chosen A ' + newArray[1] + ' Movie Kind With Seat Type' + newArray[2] + ' And The Number Of Seats is ' + newArray[3];
 
     // }
-}
+} // Ending of renderMovie() function 
 
 function handleSubmit(event) {
 
@@ -96,21 +94,26 @@ function handleSubmit(event) {
 
     var moviesSelect = movies.moviesSelect.value;
     console.log('moviesSelect ', moviesSelect);
+
     if (moviesSelect === "romantic") {
-        alert('romantic');
-        romanticDRop();
+        // alert('romantic');
+        romanticDRop();                             /// Drop down list for selected type of movies 
 
     } else if (moviesSelect === "horror") {
         // alert('horror');
+        horrorDRop();
 
     } else if (moviesSelect === "comedy") {
         // alert('comedy');
-
+        comedyDRop();
     } else if (moviesSelect === "action") {
         // alert('action');
+        actionDRop();
 
     } else if (moviesSelect === "drama") {
         // alert('drama');
+        dramaDRop();
+
     } else {
 
         alert('Please Choose Movie Type');
@@ -146,7 +149,7 @@ movieForm.addEventListener('submit', handleSubmit);
 //   console.log('MoviesKind.moviesList : ', MoviesKind.moviesList);
 
 
-/////////// Drop Down list //////////////
+////////////////////////////////////////////// Drop Down list //////////////////////////////////////////////////////////
 
 function romanticDRop() {
 
@@ -163,16 +166,103 @@ function romanticDRop() {
         theOption.value = romanticMo[i];
     }
 
-}
+} /// Ending of romanticDRop() function
+
+function horrorDRop(){
+
+    //Done: Add an <option> tag inside the form's select for each product
+
+    var selectElement = document.getElementById('items');           // drop down list 
+
+    for (var i = 0; i < horrorMo.length; i++) {
+
+        var currentProduct = horrorMo[i];
+        var theOption = document.createElement('option');
+        selectElement.appendChild(theOption);
+        theOption.textContent = currentProduct;
+        theOption.value = horrorMo[i];
+    }
+}/// Ending of horrorDRop() function
+
+function comedyDRop(){
+
+    //Done: Add an <option> tag inside the form's select for each product
+
+    var selectElement = document.getElementById('items');           // drop down list 
+
+    for (var i = 0; i < comedyMo.length; i++) {
+
+        var currentProduct = comedyMo[i];
+        var theOption = document.createElement('option');
+        selectElement.appendChild(theOption);
+        theOption.textContent = currentProduct;
+        theOption.value = comedyMo[i];
+    }
+}/// Ending of comedyDRop() function
+
+function actionDRop(){
+
+    //Done: Add an <option> tag inside the form's select for each product
+
+    var selectElement = document.getElementById('items');           // drop down list 
+
+    for (var i = 0; i < actionMo.length; i++) {
+
+        var currentProduct = actionMo[i];
+        var theOption = document.createElement('option');
+        selectElement.appendChild(theOption);
+        theOption.textContent = currentProduct;
+        theOption.value = actionMo[i];
+    }
+}/// Ending of actionDRop() function
+
+function dramaDRop(){
+
+    //Done: Add an <option> tag inside the form's select for each product
+
+    var selectElement = document.getElementById('items');           // drop down list 
+
+    for (var i = 0; i < dramaMo.length; i++) {
+
+        var currentProduct = dramaMo[i];
+        var theOption = document.createElement('option');
+        selectElement.appendChild(theOption);
+        theOption.textContent = currentProduct;
+        theOption.value = dramaMo[i];
+    }
+}/// Ending of dramaDRop() function 
+
+
 
 /////// Show up the movies types 
 var selectElement = document.getElementById('movieType1'); 
 
 selectElement.addEventListener('change',
-    function () {
-        document.getElementById('showorhidden').style.display = "inline";
-        console.log(' hi ');
-        romanticDRop();
+    function (event) {
+
+            document.getElementById('showorhiddenRomantic').style.display = "inline";
+            event.preventDefault();
+            // get all the values from the form
+            var movies = event.target;
+            console.log('movies : ', movies);
+
+            var moviesSelect = movies.value;
+            console.log('moviesSelect ', moviesSelect);
+
+            if (moviesSelect === 'romantic')
+            {
+                romanticDRop();   
+            }else if (moviesSelect === 'comedy')
+            {
+                comedyDRop();   
+            }else if (moviesSelect === 'action')
+            {
+                actionDRop();   
+            }else if (moviesSelect === 'horror')
+            {
+                dramaDRop();   
+            }
+             
     },
     false
 );
